01136b791ebe5aca81556a625b980a8fe6e3680f
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2022-01-23 20:41:05 -0300

    fix: adding dependency in createAplication() factory

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Application = require('./src/Application');
+const MiddlewareStore = require('./src/MiddlewareStore');
 
 function createApplication () {
-    return new Application();
+    return new Application(new MiddlewareStore());
 }
 
 module.exports = createApplication;
